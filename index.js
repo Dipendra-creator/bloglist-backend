d3de94aa87175d8d1adff57b5cff40d79b47601c
dipendra-react <66115431+Dipendra-creator@users.noreply.github.com>
2021-07-01 16:43:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const http = require('http')
+let date_ob = new Date();
+
+// Third Party Dependencies
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+var cors = require('cors')
+var mongoose = require("mongoose");
+
+//Middlewares
+app.use(express.json());
+app.use(cors())
+app.use(express.static('build'))
+
+// Environmental Variables
+require('dotenv').config()
+
+morgan.token('ob', function (req) {
+    return `${JSON.stringify(req.body)}`
+})
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        req.method === 'POST' ? JSON.stringify(req.body) : ''
+    ].join(' ')
+}))
+
+app.use(morgan(':method :url :status :response-time :req[header] :ob'))
+
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
